5612d63f029212e8ad488f1708f5d1c3c117a188
shettisanthosh <shettisanthosh6@gmail.com>
2026-07-14 22:45:36 +0530

    Time: 70 ms (18.85%) | Memory: 19.3 MB (54.77%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.py
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -1,8 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res=[]
         for i in range(n):
-            nums[i]+=(nums[n+i]%1024)*1024
-        for i in range(n-1,-1,-1):
-            nums[2*i+1]=nums[i]//1024
-            nums[2*i]=nums[i]%1024
-        return nums
+            res.append(nums[i])
+            res.append(nums[n+i])
+        return res
