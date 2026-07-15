1f6c030b59549417cef3b41ced2794cabffa6aec
shettisanthosh <shettisanthosh6@gmail.com>
2026-07-16 00:35:01 +0530

    Update count-subarrays-with-score-less-than-k.py

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.py
@@ -1,11 +1,12 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        total=0;result=0;i=0;j=0;n=len(nums)
+        #sliding window template
+        total=0;result=0;i=0;j=0;n=len(nums) #variables declared in 1 line on purpose
         while j<n:
             total+=nums[j]
-            while i<=j and total*(j-i+1)>=k:
-                total-=nums[i]
-                i+=1
-            result+=(j-i+1)
+            while i<=j and total*(j-i+1)>=k: #shrink the window until it gets valid
+                total-=nums[i] 
+                i+=1 
+            result+=(j-i+1) #hence it is valid, discover how many valid subarrays exists within that subarray
             j+=1
         return result
