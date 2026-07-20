2f8fcce8a2f8f3ffb697a9304bf1b298182c7a72
shettisanthosh <shettisanthosh6@gmail.com>
2026-07-20 23:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (15.97%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,9 +1,8 @@
-from collections import Counter
 class Solution:
     def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        di=dict(Counter(stones))
-        count=0
-        for c in jewels:
-            if c in di:
-                count+=di[c]
-        return count
+        jew=set(jewels)
+        c=0
+        for s in stones:
+            if s in jew:
+                c+=1
+        return c
