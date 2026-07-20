1974cfca2dd55d5dcd257e2a08288a6330410474
shettisanthosh <shettisanthosh6@gmail.com>
2026-07-20 15:36:57 +0530

    Time: 15 ms (27.16%) | Memory: 19.6 MB (81.69%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.py
+++ 1386-shift-2d-grid/shift-2d-grid.py
@@ -1,18 +1,14 @@
 class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        if not k:
-            return grid
-        r,c=len(grid),len(grid[0])
-        n=r*c
-        k%=n
-        if not k:
-            return grid
-        def shift(i,j):
-            while i<j:
-                grid[i//c][i%c],grid[j//c][j%c]=grid[j//c][j%c],grid[i//c][i%c]
-                i+=1
-                j-=1
-        shift(0,n-1)
-        shift(0,k-1)
-        shift(k,n-1)
-        return grid
+        m,n=len(grid),len(grid[0])
+        def posToVal(r,c):
+            return r*n+c
+        def valToPos(v):
+            return [v//n, v%n]
+        res=[[0]*n for i in range(m)]
+        for r in range(m):
+            for c in range(n):
+                newVal=(posToVal(r,c)+k)%(m*n)
+                newr,newc=valToPos(newVal)
+                res[newr][newc]=grid[r][c]
+        return res
