17abd1cf43c6c1302a57519808fe0640d39307e7
shettisanthosh <shettisanthosh6@gmail.com>
2026-07-23 22:01:46 +0530

    Time: 4 ms (55.84%) | Memory: 46.9 MB (51.81%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.java
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -1,11 +1,7 @@
 class Solution {
     public int[] transformArray(int[] nums) {
         for(int i=0;i<nums.length;i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }else{
-                nums[i]=1;
-            }
+            nums[i]=nums[i]&1;
         }
         Arrays.sort(nums);
         return nums;
