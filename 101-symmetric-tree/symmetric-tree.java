95a6a4062331baef3da6d89137c96dba026621a9
shettisanthosh <shettisanthosh6@gmail.com>
2026-07-31 00:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (33.86%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,30 +15,21 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-         if(root==null){
+        if(root==null){
             return true;
         }
-        Queue<TreeNode>leftTree= new LinkedList<>();
-        Queue<TreeNode>rightTree= new LinkedList<>();
-        leftTree.offer(root.left);
-        rightTree.offer(root.right);
-        while(!leftTree.isEmpty() && !rightTree.isEmpty()){
-            TreeNode leftNode=leftTree.poll();
-            TreeNode rightNode = rightTree.poll();
-            if(leftNode==null && rightNode==null){
-                continue;
-            }
-             if(leftNode==null || rightNode==null){
-                return false;
-            }
-            if(leftNode.val!=rightNode.val){
-                return false;
-            }
-            leftTree.offer(leftNode.left);
-            leftTree.offer(leftNode.right);
-            rightTree.offer(rightNode.right);
-            rightTree.offer(rightNode.left);
+        return mirror(root.left,root.right);
+    }
+    boolean mirror(TreeNode l, TreeNode r){
+        if(l==null && r==null){
+            return true;
         }
-        return true;
+        if(l==null || r==null){
+            return false;
+        }
+        if(l.val!=r.val){
+            return false;
+        }
+        return mirror(l.left,r.right) && mirror(l.right,r.left);
     }
 }
