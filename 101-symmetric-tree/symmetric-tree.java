26c7196834d581b9293d8444481952402f765fd7
shettisanthosh <shettisanthosh6@gmail.com>
2026-07-31 00:24:01 +0530

    Update symmetric-tree.java

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -13,6 +13,7 @@
  *     }
  * }
  */
+// Approach 1 normally done by using recursion
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root==null){
@@ -31,5 +32,35 @@ class Solution {
             return false;
         }
         return mirror(l.left,r.right) && mirror(l.right,r.left);
+    }
+}
+// Approach 2 solved using Iterative way using 2 Queues basically Level Order Traversal
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+         if(root==null){
+            return true;
+        }
+        Queue<TreeNode>leftTree= new LinkedList<>();
+        Queue<TreeNode>rightTree= new LinkedList<>();
+        leftTree.offer(root.left);
+        rightTree.offer(root.right);
+        while(!leftTree.isEmpty() && !rightTree.isEmpty()){
+            TreeNode leftNode=leftTree.poll();
+            TreeNode rightNode = rightTree.poll();
+            if(leftNode==null && rightNode==null){
+                continue;
+            }
+             if(leftNode==null || rightNode==null){
+                return false;
+            }
+            if(leftNode.val!=rightNode.val){
+                return false;
+            }
+            leftTree.offer(leftNode.left);
+            leftTree.offer(leftNode.right);
+            rightTree.offer(rightNode.right);
+            rightTree.offer(rightNode.left);
+        }
+        return true;
     }
 }
