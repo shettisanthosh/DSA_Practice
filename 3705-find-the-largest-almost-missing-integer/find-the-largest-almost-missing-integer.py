d88baf82f2cd5b2b6cfe9e262a9f79f72a837771
shettisanthosh <shettisanthosh6@gmail.com>
2026-08-18 10:25:15 +0530

    Time: 2 ms (65.16%) | Memory: 19.4 MB (35.66%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
@@ -1,13 +1,12 @@
+from collections import Counter
 class Solution:
     def largestInteger(self, nums: List[int], k: int) -> int:
         n=len(nums)
-        count=[0]*51
-        for i in range(n-k+1):
-            curr=nums[i:i+k]
-            uniq=set(curr)
-            for ele in uniq:
-                count[ele]+=1
-        for i in range(50,-1,-1):
-            if count[i]==1:
-                return i
-        return -1
+        if k==n:
+            return max(nums)
+        count=Counter(nums)
+        if k==1:
+            arr=[x for x in nums if count[x]==1]
+        else:
+            arr=[x for x in (nums[0],nums[-1]) if count[x]==1] 
+        return max(arr) if arr else -1
