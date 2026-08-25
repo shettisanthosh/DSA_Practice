ff094cf3b09d3d80223a418c4cec4b88530143ab
shettisanthosh <shettisanthosh6@gmail.com>
2026-08-25 11:42:34 +0530

    Time: 3 ms (20.84%) | Memory: 19.3 MB (53.35%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -1,10 +1,8 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
-        count=[0]*1000000
-        for i in range(len(nums)):
-            if nums[i]%k==0:
-                count[nums[i]]=1
-        for i in range(k,1000000,k):
-            if count[i]==0:
-                return i
-        return -1
+        multiples={num for num in nums if num%k==0}
+        current=k
+        while True:
+            if current not in multiples:
+                return current
+            current+=k
