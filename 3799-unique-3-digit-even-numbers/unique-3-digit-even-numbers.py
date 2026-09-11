75b93291f2c0a00a7a6abbe6ac5618b5d2818192
shettisanthosh <shettisanthosh6@gmail.com>
2026-09-11 16:15:43 +0530

    Time: 11 ms (84.40%) | Memory: 19.4 MB (20.44%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,22 +1,4 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        freq=[0]*10
-        for d in digits:
-            freq[d]+=1
-        ans=0
-        for first in range(1,10):
-            if freq[first]==0:
-                continue
-            freq[first]-=1
-            for second in range(10):
-                if freq[second]==0:
-                    continue
-                freq[second]-=1
-                ans += (freq[0] > 0)
-                ans += (freq[2] > 0)
-                ans += (freq[4] > 0)
-                ans += (freq[6] > 0)
-                ans += (freq[8] > 0)
-                freq[second]+=1
-            freq[first]+=1
-        return ans
+        return  sum(1  for a,b,c in {*permutations(digits,3)} if a and c&1==0)
+        
